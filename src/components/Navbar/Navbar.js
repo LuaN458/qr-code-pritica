64dcdc2f0bc0a56b7d0880d291e65d00a00e81cd
Luan Henrique <luan.lh34@gmail.com>
2025-09-30 20:18:11 -0300

    Feito um objeto para checkar si a classe está ativa oui não, salvo no localStorage, e carregada de volta ao atualizar a página no useEffect, chave name está com provisório

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,50 +5,68 @@ export default function NavBar() {
 
   const classN = 'body-bg-dark';
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const isActive = {
     active: false,
   }
+
+  const checkClass = { name: false };
 
   const handleClick = (e) => {
 
     const bodyBg = document.querySelector("body");
     const btn = document.querySelector(".btn-dark");
 
-    if (bodyBg.classList.contains(`.${classN}`)) {
-      bodyBg.classList.remove(classN);
-    }
     if (isActive.active) {
       bodyBg.classList.remove(classN);
       btn.textContent = "Escurecer";
+      checkClass.name = false;
     }
 
     if (!isActive.active) {
       bodyBg.classList.add(classN);
       btn.textContent = "Clarear";
+      checkClass.name = true;
     }
 
     isActive.active = !isActive.active;
+
+    const classTxt = JSON.stringify(checkClass);
+
+    localStorage.setItem('classObj', classTxt);
   }
 
   React.useEffect(() => {
+
     const bodyBg = document.querySelector("body");
     const btn = document.querySelector(".btn-dark");
 
-    if(bodyBg.classList.contains(classN)) {
+    if (bodyBg.classList.contains(classN)) {
       btn.textContent = "Clarear";
     }
 
-    btn.textContent = "Escurecer";
-  }, []);
+    const classObj = localStorage.getItem('classObj');
+
+    const ObjClass = JSON.parse(classObj);
+
+    const { name } = ObjClass;
+
+    if (name) {
+      bodyBg.classList.add(classN);
+      btn.textContent = "Clarear";
+      isActive.active = !isActive.active;
+    } else {
+      btn.textContent = "Escurecer";
+    }
+
+  }, [isActive]);
 
   return (
     <Container>
       <h1>
         HOME
       </h1>
-
       <button type="button" onClick={e => handleClick(e)} className='btn-dark'></button>
-
     </Container>
   );
 }
